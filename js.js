f0bc8caf3869c0cc1e360317c3c1d277b1179cff
Sveldevorls <64679979+Sveldevorls@users.noreply.github.com>
2024-11-26 23:10:02 +0800

    Add display logic

--- js.js
+++ js.js
@@ -1,22 +1,28 @@
 let currentNumberRawString = "0";
 let currentNumber = 0;
+let processQueue = [0]
 let isFloat = false;
-let processQueue = [];
+let prevActiveOperand = null;
+
+let operations = {
+    "plus" : "+",
+    "minus" : "-",
+    "multiply" : "*",
+    "divide" : "/",
+    "equals" : "=",
+    /* "clear" : clearDisplay,
+    "sign" : changeSign,
+    "backspace" : backspace, */
+}
 
 let upperDisplay = document.body.querySelector(".upper");
 let mainDisplay = document.body.querySelector(".main");
 
-
 let numberButtons = document.body.querySelectorAll(".num");
-for (numberButton of numberButtons) {
-    numberButton.addEventListener("click", e => {
-        let currDigit = e.target.innerText;
-        isFloat ? renderFloat(currDigit) : renderInt(currDigit);
-    })
-}
-
-
+let operandButtons = document.body.querySelectorAll(".operand");
 let decimalPoint = document.body.querySelector(".decimal");
+
+
 decimalPoint.addEventListener("click", () => {
     if (!isFloat) {
         isFloat = true;
@@ -25,34 +31,117 @@ decimalPoint.addEventListener("click", () => {
     }
 })
 
+for (numberButton of numberButtons) {
+    numberButton.addEventListener("click", e => {
+        let currDigit = e.target.innerText;
+        renderNumber(currDigit);
+    })
+}
 
-let operandButtons = document.body.querySelectorAll(".operand");
 for (operandButton of operandButtons) {
     operandButton.addEventListener("click", e => {
-        // 0 -> first number, 2 -> second number for operands taking 2 arguments
-        if (processQueue.length == 0 || processQueue.length == 2) processQueue.push(currentNumber)
-        
-        // 1 -> overwriting operand
-        else if (processQueue.length == 1) 1==1
-        
+        let operationPressed = e.target;
+        // clear, sign, backspace can be called anytime
+        if (operationPressed.id == "clear" || operationPressed.id == "sign" || operationPressed.id == "backspace") {
+            operations[operationPressed.id].call();
+        } 
+
+        else {
+            switch (processQueue.length) {
+                // 1 -> add operand to queue if in +-*/
+                case 1:
+                    if (operationPressed.id != "equals") {
+                        processQueue.push(operations[operationPressed.id])
+                        showCurrentOperand(operationPressed);
+                        renderUpperDisplay();
+                        currentNumberRawString = "0";
+                        currentNumber = 0;
+                        isFloat = false;
+                        mainDisplay.innerText = currentNumberRawString;
+                    }
+                    break
+                
+                // 2 -> change current operand if not equals, eval if equals
+                case 2:
+                    if (operationPressed.id != "equals") {
+                        processQueue[1] = operations[operationPressed.id]
+                        showCurrentOperand(operationPressed);
+                        renderUpperDisplay();
+                    } else {
+                        processQueue.push(currentNumber)
+                        operate(processQueue)
+                    }
+                    break;
+
+                // 3 -> eval first, if operand in +-*/ then take the result as the first number of the new queue
+                case 3:
+                    operate(processQueue)
+                    if (operationPressed.id != "equals") {
+                        //
+                    }
+                    break
+            }
+        }
     })
 }
 
 
-function renderInt(nStr) {
-    if (currentNumberRawString == "0") {
-        if (nStr != "0") {
-            currentNumberRawString = nStr
-        } 
-    } else {
-        currentNumberRawString += nStr;
-    }
-    currentNumber = parseInt(currentNumberRawString, 10);
+
+
+
+
+function renderNumber(dStr) {
+    currentNumberRawString == "0" ? currentNumberRawString = dStr :
+                                    currentNumberRawString += dStr;
+    isFloat ? currentNumber = parseFloat(currentNumberRawString, 10) :
+              currentNumber = parseInt(currentNumberRawString, 10);
     mainDisplay.innerText = currentNumberRawString;
+    updateQueueNumber(currentNumber);
+    console.log(processQueue)
 }
 
-function renderFloat(nStr) {
-    currentNumberRawString += nStr;
-    currentNumber = parseFloat(currentNumberRawString, 10);
-    mainDisplay.innerText = currentNumberRawString;
+function updateQueueNumber(number) {   
+    switch (processQueue.length) {
+        // 1, 3 -> update number (first number, second number)
+        case 1:
+        case 3:
+            processQueue[processQueue.length - 1] = currentNumber;
+            break
+
+        // 2 -> add number to the queue (operation selected)
+        case 2:
+            processQueue.push(number);
+            break
+    }
+    if (prevActiveOperand) prevActiveOperand.classList.remove("active");
+}
+
+function showCurrentOperand(button){
+    if (prevActiveOperand != null) prevActiveOperand.classList.remove("active")
+        button.classList.add("active");
+        prevActiveOperand = button;
+}
+
+function renderUpperDisplay() {
+    upperDisplay.innerText = processQueue.join(" ");
+}
+
+function operate(queue) {
+    let [num1, op, num2] = [...queue];
+    upperDisplay.innerText = queue.join(" ") + " =";
+    switch (op) {
+        case "+":
+            mainDisplay.innerText = (num1 + num2).toString();
+            break
+        case "-":
+            mainDisplay.innerText = (num1 - num2).toString();
+            break
+        case "*":
+            mainDisplay.innerText = (num1 * num2).toString();
+            break
+        case "/":
+            if (num2 == 0) mainDisplay.innerText = "ERROR"
+            else mainDisplay.innerText = (num1 / num2).toString();
+            break
+    }
 }
